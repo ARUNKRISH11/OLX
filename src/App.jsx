4540fc744a79bd2f47cbfe018cc93f4e60015da9
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-07-08 16:20:06 +0530

    install dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter as Router,Routes, Route, Link} from "react-router-dom";
+
+import './App.css';
+
+/**
+ * ?  =====Import Components=====
+ */
+import Home from './Pages/Home';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route>
+              <App />
+          </Route>
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
