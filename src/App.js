b048a83a4a33b035f97f7f770f5a594d2c44c4f4
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2024-07-08 21:01:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router,Routes, Route, Link} from "react-router-dom";
+
 import './App.css';
 
-/**
- * ?  =====Import Components=====
- */
+
+// =====Import Components=====
+ 
 import Home from './Pages/Home';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} >
+          </Route>
+          <Route path="/signup" element={<Signup/>}>
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
